7980fd2670f9c8096e630123dafb4a05bd629fcf
muchstarlight <muchstarlight@163.com>
2026-01-03 19:51:14 +0800

    fix(修了一下action顺序问题)

--- src/components/ArchivePanel.svelte
+++ src/components/ArchivePanel.svelte
@@ -3,8 +3,8 @@ import { onMount } from "svelte";
 
 import I18nKey from "../i18n/i18nKey";
 import { i18n } from "../i18n/translation";
-import { getPostUrlBySlug } from "../utils/url-utils";
 import type { PostForList } from "../utils/content-utils";
+import { getPostUrlBySlug } from "../utils/url-utils";
 
 export interface Props {
 	tags?: string[];
